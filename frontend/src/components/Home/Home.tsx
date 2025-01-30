d43da2f7006296a77099e7a184e43e6cdee8df94
Jessica Pabst <jess@Lorens-MacBook-Pro.local>
2025-01-30 15:48:54 -0700

    finished design, work on api calls next

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,7 +1,29 @@
+import './Home.scss';
+
 export default function Home() {
+
+ let adviceNumber = '10';
+
+ let advice = 'Flop over attack feet behind the couch hide when guests come over '
+
+ let patternDividerMobile = '/images/pattern-divider-mobile.svg'
+
+ // need to create a toggle here for when the screen width hits a certain size? //
+ let patternDividerDesktop = '/images/pattern-divider-desktop.svg'
+
  return (
-  <div>
-   <p>Home page</p>
+  <div className='main-div'>
+   <div className='advice-container'>
+    <h1>ADVICE #{adviceNumber}</h1>
+    <p>"{advice}"</p>
+    <picture>
+     <source media="(min-width: 1440px)" srcSet={patternDividerDesktop} />
+     <img src={patternDividerMobile} alt="Pattern Divider" />
+    </picture>
+   </div>
+   <div className='gen-bttn'>
+    <img src='/images/icon-dice.svg' />
+   </div>
   </div>
  )
 }
